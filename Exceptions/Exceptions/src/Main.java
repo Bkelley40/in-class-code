2c7dc5596f2e449572c6428ed78cd09044c859a3
Steven Bitner <sbitner@uwf.edu>
2019-02-12 15:47:48 -0600

    Exceptions lecture 1

---
+++ Exceptions/Exceptions/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main (String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		int value;
+		do
+		{
+		try
+		{
+			value = getInt(keyboard);
+		}
+		catch (NegativeNumberException foo)
+		{
+			System.out.println("Not a positive integer.");
+			System.out.println(foo.getMessage());
+		}
+		catch (Exception e)
+		{
+			System.out.println("Invalid value");
+			break;
+		}
+		} while (true);
+	}
+
+	public static int getInt (Scanner keyboard) throws NegativeNumberException
+	{
+		System.out.print("Enter a positive integer: ");
+		
+		int input = keyboard.nextInt();
+		System.out.println("Entered: "+input);
+		checkIsPositive(input);
+		return input;
+	}
+	
+	private static void checkIsPositive (int input) throws NegativeNumberException
+	{
+		if (input < 0)
+		{
+			throw new NegativeNumberException("    "+input+" is not a positive integer.");
+		}
+	}
+}
