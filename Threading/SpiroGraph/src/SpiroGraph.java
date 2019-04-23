f566ef21752e17c74edab552d6f866a34f9553a3
Steven Bitner <sbitner@uwf.edu>
2019-04-23 15:06:21 -0500

    Adding basic oval

--- Threading/SpiroGraph/src/SpiroGraph.java
+++ Threading/SpiroGraph/src/SpiroGraph.java
@@ -1,12 +1,16 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class SpiroGraph extends JFrame implements ActionListener
 {
+	private JPanel box;
 	public static void main (String[] args)
 	{
 		SpiroGraph wow = new SpiroGraph();
@@ -17,16 +21,28 @@ public class SpiroGraph extends JFrame implements ActionListener
 	{
 		super("BOOM");
 		
+		this.box = new JPanel();
+		this.add(this.box);
+		
 		this.setSize(400, 400);
 		JButton go = new JButton("GO!");
 		go.addActionListener(this);
 		this.add(go, BorderLayout.SOUTH);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 	@Override
 	public void actionPerformed (ActionEvent e)
 	{
-		System.out.println("heyo");
+		this.fill();
+	}
+	
+	public void fill ()
+	{
+		Graphics2D g2 = (Graphics2D)this.getGraphics();
+		g2.setColor(new Color(255, 0 ,0));
+		
+		g2.drawOval(50,  180, 300,  40);
 	}
 
 }
